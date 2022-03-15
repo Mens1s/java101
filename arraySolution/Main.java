8baf79135e256832a0335b5f6c3990b81a4c8b4b
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2022-03-15 23:15:41 +0300

    Update Main.java

--- arraySolution/Main.java
+++ arraySolution/Main.java
@@ -2,7 +2,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {1,11,22,33,44,55,66,77,88,99};
+        int[] arr = {1,11,22,22,22,55,66,77,88,1};
         System.out.println(HelperArray.average(arr));
+        HelperArray.find(arr,55);
+        HelperArray.star();
+        HelperArray.repeat(arr);
     }
 }
