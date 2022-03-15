6021f3e63a5cf1885d8c2735c297e18c598cc759
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2022-03-15 23:15:48 +0300

    Update HelperArray.java

--- arraySolution/HelperArray.java
+++ arraySolution/HelperArray.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class HelperArray {
     static void print(int[] arr){
         for(int i= 0; i < arr.length ;i++){
@@ -19,11 +20,56 @@ public class HelperArray {
     }
 
     static double average(int[] l1){
-        double temp = 0;
-        for(int num : l1){
-            temp += num;
+        double res=0;
+
+        for(int i=0;i<l1.length;i++) {
+            res+=1.0/l1[i];
         }
-        return temp/l1.length;
+        return l1.length/res;
     }
 
+    static void find(int[] l1 ,int num){
+        Arrays.sort(l1);
+        int small = l1[0], big = l1[l1.length-1];
+        for(int i = 0; i < l1.length ; i++){
+            if(num<l1[i] && big > l1[i]) big = l1[i];
+            if(num>l1[i] && small < l1[i]) small = l1[i];
+        }
+        System.out.println("The nearest big number : "+small);
+        System.out.println("The nearest small number : "+big);
+    }
+
+    static void star(){
+        String[][] arr = new String[6][4];
+
+        for(int i = 0; i<arr.length;i++){
+            for(int a = 0 ; a < arr[i].length; a++){
+                if(i == 0 || i==2 || a == 0 || a == 3){
+                    arr[i][a] = " * ";
+                } else{
+                    arr[i][a] = "   ";
+                }
+            }
+        }
+        for(String[] a : arr){
+            for(String b : a){
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+    }
+
+    static void repeat(int[] arr){
+        Arrays.sort(arr);
+        int temp=arr[arr.length-1];
+        int counter = 0;
+        for(int i : arr){
+            if(temp == i && counter==0 && i%2==0){
+                System.out.println(i+" is repeat!");
+                counter++;
+            }
+            else counter = 0;
+            temp = i;
+        }
+    }
 }
